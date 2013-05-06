9f01fa1f345fd59beabc6451402ea66e08f180f1
Jaromir Tomek <jaromir.tomek@gmail.com>
2013-05-06 14:48:15 +0200

    Added default class name in SearchInput

--- app/widgets/search_input.php
+++ app/widgets/search_input.php
@@ -1,4 +1,16 @@
 <?php
 class SearchInput extends TextInput{
 	var $input_type = 'search';
+
+	function SearchInput($options = array()){
+		$options += array(
+			"attrs" => array()
+		);
+
+		$options["attrs"] += array(
+			"class" => "search-query"
+		);
+
+		parent::TextInput($options);
+	}
 }
