62a1e139ad6b9bc13bd98b0f26bc42405320eb31
Bohdan Ganicky <bohdan.ganicky@gmail.com>
2013-11-04 18:10:09 +0100

    Generic admin javascript template.

--- public/dist/admin/js/app.js
+++ public/dist/admin/js/app.js
@@ -11705,59 +11705,20 @@ var ATK14 = (function( $, window, undefined ) {
 (function( window, $, undefined ) {
 	var document = window.document,
 
-	SKELET = {
+	ADMIN = {
 		common: {
 			init: function() {
 				// application-wide code
 			}
 		},
 
-		users: {
+		logins: {
 			init: function() {
 				// controller-wide code
 			},
 
-			Xcreate_new: function() {
+			create_new: function() {
 				// action-specific code
-
-				/*
-				 * Check whether login is available.
-				 * Simple demo of working with an API.
-				 */
-				var $login = $( "#id_login" ),
-					$icon = $( "<span class='glyphicon'></span>" ).insertAfter( $login );
-
-				$login.on( "change", function() {
-					// Login input value to check.
-					var value = $login.val(),
-					// API URL.
-						url = "/api/" + $( "html" ).attr( "lang" ) + "/login_availabilities/detail/",
-					// GET values for API. Available formats: xml, json, yaml, jsonp.
-						data = {
-							login: value,
-							format: "json"
-						};
-
-					// AJAX request to the API.
-					if ( value !== "" ) {
-						$.ajax({
-							dataType: "json",
-							url: url,
-							data: data,
-							success: function( json ) {
-								$icon.attr( "title", json.status );
-
-								if ( json.status !== "available" ) {
-									$icon.removeClass( "glyphicon-ok" ).addClass( "glyphicon-remove" );
-								} else {
-									$icon.removeClass( "glyphicon-remove" ).addClass( "glyphicon-ok" );
-								}
-							}
-						});
-					} else {
-						$icon.removeClass( "glyphicon-ok glyphicon-remove" ).attr( "title", "" );
-					}
-				}).change();
 			}
 		}
 	};
@@ -11767,9 +11728,9 @@ var ATK14 = (function( $, window, undefined ) {
 	 * Garber-Irish DOM-based routing.
 	 * See: http://goo.gl/z9dmd
 	 */
-	SKELET.UTIL = {
+	ADMIN.UTIL = {
 		exec: function( controller, action ) {
-			var ns = SKELET;
+			var ns = ADMIN;
 
 			if ( action === undefined ) {
 				action = "init";
@@ -11785,15 +11746,15 @@ var ATK14 = (function( $, window, undefined ) {
 			controller = body.getAttribute( "data-controller" ),
 			action = body.getAttribute( "data-action" );
 
-			SKELET.UTIL.exec( "common" );
-			SKELET.UTIL.exec( controller );
-			SKELET.UTIL.exec( controller, action );
+			ADMIN.UTIL.exec( "common" );
+			ADMIN.UTIL.exec( controller );
+			ADMIN.UTIL.exec( controller, action );
 		}
 	};
 
-	// Expose SKELET to the global object.
-	window.SKELET = SKELET;
+	// Expose ADMIN to the global object.
+	window.ADMIN = ADMIN;
 
 	// Initialize application.
-	$( document ).ready( SKELET.UTIL.init );
+	$( document ).ready( ADMIN.UTIL.init );
 })( window, window.jQuery );
