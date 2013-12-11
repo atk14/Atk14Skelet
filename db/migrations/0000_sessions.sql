618148645d1d07b4f0fad4f39829ab369ceb7fe7
Jaromir Tomek <jaromir.tomek@gmail.com>
2013-12-11 09:58:29 +0100

    New session tables structure

--- db/migrations/0000_sessions.sql
+++ db/migrations/0000_sessions.sql
@@ -1,9 +1,10 @@
 CREATE SEQUENCE seq_sessions;
 CREATE TABLE sessions(
         id INT NOT NULL PRIMARY KEY DEFAULT NEXTVAL('seq_sessions'),
-        security VARCHAR(32) NOT NULL CHECK (security ~ '^[a-zA-Z0-9]{32}$'),
+        security VARCHAR(255) NOT NULL,
+        session_name VARCHAR(255) NOT NULL,
         --
-        remote_addr VARCHAR(255) DEFAULT '' NOT NULL,
+        remote_addr VARCHAR(255) NOT NULL,
         --
         created TIMESTAMP DEFAULT NOW() NOT NULL,
         last_access TIMESTAMP DEFAULT NOW() NOT NULL
@@ -14,13 +15,13 @@ CREATE SEQUENCE seq_session_values;
 CREATE TABLE session_values(
         id INT NOT NULL PRIMARY KEY DEFAULT NEXTVAL('seq_session_values'),
         session_id INT NOT NULL,
-        session_name VARCHAR(64) NOT NULL CHECK(LENGTH(session_name)>0),
+        section VARCHAR(255) NOT NULL,
         --
-        key VARCHAR(128) NOT NULL CHECK(LENGTH(key)>0),
-        value TEXT DEFAULT '' NOT NULL,
-        expiration TIMESTAMP DEFAULT NULL,
+        key VARCHAR(255) NOT NULL,
+        value TEXT,
+        expiration TIMESTAMP,
         --
-        CONSTRAINT unq_sessionvalues UNIQUE(session_id,session_name,key),
+        CONSTRAINT unq_sessionvalues UNIQUE(session_id,section,key),
         CONSTRAINT fk_sessionvalues_sessions FOREIGN KEY (session_id) REFERENCES sessions(id) ON DELETE CASCADE
 );
 CREATE INDEX in_sessionvalues_sessionid ON session_values(session_id);
