44a57b4ed4080bde362f5f52719c039cc4515f3b
Jaromir Tomek <jaromir.tomek@gmail.com>
2014-06-17 10:37:15 +0200

    SearchInput fixed

--- app/widgets/search_input.php
+++ app/widgets/search_input.php
@@ -1,15 +1,16 @@
 <?php
 class SearchInput extends TextInput{
-	var $input_type = 'search';
+	var $input_type = 'search'; //  renders <input type="search"...>
 
 	function __construct($options = array()){
+		/*
 		$options += array(
 			"attrs" => array()
 		);
 
 		$options["attrs"] += array(
-			"class" => "search-query"
-		);
+			"class" => "text form-control search-query" // ... when one needs a special class (search-query)
+		); */
 
 		parent::__construct($options);
 	}
