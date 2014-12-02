685cab986e6fe4ddc1e1337cdd4332bb4b32a3a5
Jaromir Tomek <jaromir.tomek@gmail.com>
2014-12-02 11:06:31 +0100

    Improved auto configuration of forms

--- app/forms/application_form.php
+++ app/forms/application_form.php
@@ -9,8 +9,8 @@ class ApplicationForm extends Atk14Form{
 	 * 
 	 */
 	function pre_set_up(){
-		// submit method GET is automatically set in IndexForm or DetailForm
-		if(preg_match('/^(Index|Detail)Form$/i',get_class($this))){
+		// submit method GET is automatically set in IndexForm or DetailForm (or Users\IndexForm, Users\DetailForm, ...)
+		if(preg_match('/^(|.*\\\\)(Index|Detail)Form$/i',get_class($this))){ 
 			$this->set_method("get");
 		}
 	}
@@ -32,7 +32,7 @@ class ApplicationForm extends Atk14Form{
 	/**
 	 * Has this form only a few fields and so on a page it appears to be small?
 	 * 
-	 * This method is used in partial template app/views/shared/form.tpl
+	 * This method is used in partial template app/views/shared/_form.tpl
 	 */
 	function is_small(){
 		return sizeof($this->fields)<=4;
--- app/forms/rest_api_form.php
+++ app/forms/rest_api_form.php
@@ -3,13 +3,6 @@ class RestApiForm extends ApplicationForm{
 	var $has_format_field = true;
 	var $error_http_status_code = 400; // jaky HTTP kod se ma vytisknout v pripade, ze formular nebude validni...
 
-	function pre_set_up(){
-		// submit method GET is automatically set in IndexForm or DetailForm
-		if(preg_match('/^(Index|Detail)Form$/i',get_class($this))){
-			$this->set_method("get");
-		}
-	}
-
 	function post_set_up(){
 		$this->has_format_field && $this->add_format_field();
 
@@ -17,6 +10,8 @@ class RestApiForm extends ApplicationForm{
 		foreach($this->fields as $key => &$f){
 			$f->label = $key;
 		}
+
+		parent::post_set_up();
 	}
 
 	function add_format_field(){
