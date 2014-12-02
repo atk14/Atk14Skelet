31d24eb67e1f60c570a74f8774055f6cb34c2032
Jaromir Tomek <jaromir.tomek@gmail.com>
2014-12-02 10:53:18 +0100

    Text on the submit button in the password recovery form

--- app/forms/password_recoveries/recovery_form.php
+++ app/forms/password_recoveries/recovery_form.php
@@ -3,5 +3,7 @@ require_once(dirname(__FILE__)."/../users/users_form.php");
 class RecoveryForm extends UsersForm{
 	function set_up(){
 		$this->_add_password_fields();
+
+		$this->set_button_text(_("Set new password"));
 	}
 }
