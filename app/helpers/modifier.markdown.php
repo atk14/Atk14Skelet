7a38ddca0af873b4e06a82e0fce35828bdeadddd
Jaromir Tomek <jaromir.tomek@gmail.com>
2015-10-02 16:13:33 +0200

    Newer version of Markdown library. It is installed by the Composer.

--- app/helpers/modifier.markdown.php
+++ app/helpers/modifier.markdown.php
@@ -1,3 +1,7 @@
 <?php
-// smarty_modifier_markdown() is part of 3rd party library
-require_once(ATK14_DOCUMENT_ROOT."/lib/markdown.php");
+/**
+ * {!$source_text|markdown}
+ */
+function smarty_modifier_markdown($text){
+	return Michelf\MarkdownExtra::defaultTransform($text);
+}
