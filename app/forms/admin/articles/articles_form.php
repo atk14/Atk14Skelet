d488b80245c5d2d09b84a15852e5ea60f5aec54e
Jaromir Tomek <jaromir.tomek@gmail.com>
2015-10-02 16:16:31 +0200

    Refactoring

---
+++ app/forms/admin/articles/articles_form.php
@@ -0,0 +1,26 @@
+<?php
+class ArticlesForm extends AdminForm {
+	function set_up(){
+		$this->add_field("title",new CharField(array(
+			"label" => _("Title"),
+			"max_length" => 255,
+		)));
+
+		$this->add_field("body",new TextField(array(
+			"label" => _("Body"),
+			"help_text" => _("Mardown format is expected"),
+		)));
+
+		$this->add_field("published_at",new DateTimeField(array(
+			"label" => _("Published At"),
+			"initial" => time(),
+		)));
+
+		$this->add_field("tags", new TagsField(array(
+			"label" => _("Tags"),
+			"required" => false,
+			"hint" => "news , webdesign",
+			"help_text" => sprintf(_("Mention tag <em>%s</em> when this article should be displayed in the news section"),h(Tag::FindById(Tag::ID_NEWS)))
+		)));
+	}
+}
--- app/forms/admin/articles/edit_form.php
+++ app/forms/admin/articles/edit_form.php
@@ -1,3 +1,2 @@
 <?php
-require_once(dirname(__FILE__)."/create_new_form.php");
-class EditForm extends CreateNewForm{ }
+class EditForm extends ArticlesForm{ }
