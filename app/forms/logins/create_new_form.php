1e281956f93ba21e9ed1eec14ffa9ae1da22f3be
Jaromir Tomek <jaromir.tomek@gmail.com>
2016-05-10 14:11:41 +0200

    Labels added into the login form

--- app/forms/logins/create_new_form.php
+++ app/forms/logins/create_new_form.php
@@ -1,10 +1,12 @@
 <?php
 class CreateNewForm extends ApplicationForm{
 	function set_up(){
-		$this->add_field("login", new CharField(array(	
+		$this->add_field("login", new CharField(array(
+			"label" => _("Login"),
 			"max_length" => 255,
 		)));
 		$this->add_field("password", new PasswordField(array(
+			"label" => _("Password"),
 			"max_length" => 255,
 		)));
 
