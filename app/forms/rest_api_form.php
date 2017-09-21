632e5a1ee8b41ea343ee1cee481c15c1c11dcb47
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-09-21 14:08:41 +0200

    [API] Added RestApiForm::$value_or_initial_fields

--- app/forms/rest_api_form.php
+++ app/forms/rest_api_form.php
@@ -1,14 +1,15 @@
 <?php
 class RestApiForm extends ApplicationForm{
+
 	var $has_format_field = true;
-	var $error_http_status_code = 400; // jaky HTTP kod se ma vytisknout v pripade, ze formular nebude validni...
+	var $error_http_status_code = 400; // this HTTP status is returned from API in case that the form is invalid
+	var $value_or_initial_fields = array(); // when some value is missing in an API call the field initial value can be set in cleaned data; e.g. ["country_id"]
 
 	function post_set_up(){
 		$this->has_format_field && $this->add_format_field();
 
-		// automaticky prevod labelu z "Article Id" na "article_id"...
 		foreach($this->fields as $key => &$f){
-			$f->label = $key;
+			$f->label = $key; // "Article Id" -> "article_id"; it is better for API
 		}
 
 		parent::post_set_up();
@@ -28,4 +29,18 @@ class RestApiForm extends ApplicationForm{
 			"required" => !false, 
 		)));
 	}
+
+	function clean(){
+		list($err,$cleaned_values) = parent::clean();
+
+		$keys = array_keys($cleaned_values);
+		foreach($this->value_or_initial_fields as $f){
+			if(in_array($f,$keys) && !isset($cleaned_values[$f])){
+				$cleaned_values[$f] = $this->get_initial($f);
+			}
+		}
+
+		return array($err,$cleaned_values);
+	}
+
 }
