2f6c450cc14356c720c321e1c15269101bd39e6c
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-10-09 17:22:32 +0200

    Helper button_create_new improved

--- app/helpers/block.button_create_new.php
+++ app/helpers/block.button_create_new.php
@@ -7,6 +7,7 @@
  * See app/views/shared/helpers/_button_create_new.tpl
  *
  *	<h1>{button_create_new}Create new article{/button_create_new} Articles</h1>
+ *	<h1>{button_create_new _class="btn btn-primary"}Create new article{/button_create_new} Articles</h1>
  *	...
  *	<h3>{button_create_new controller=attachments article_id=$article}Add attachment{/button_create_new} Attachments</h3>
  */
@@ -18,9 +19,22 @@ function smarty_block_button_create_new($params,$content,$template,&$repeat){
 		"action" => "create_new"
 	);
 
+	$attrs = array();
+	foreach($params as $k => $v){
+		if(preg_match('/^_(.*)/',$k,$m)){
+			$attrs[$m[1]] = $v;
+			unset($params[$k]);
+		}
+	}
+
+	$attrs += array(
+		"class" => "btn btn-default",
+	);
+
 	$original_smarty_vars = $smarty->getTemplateVars();
 	$smarty->assign("title",$content);
 	$smarty->assign("create_new_url",Atk14Url::BuildLink($params));
+	$smarty->assign("attrs",$attrs);
 	$out = $smarty->fetch("shared/helpers/_button_create_new.tpl");
 	$smarty->clearAllAssign();
 	$smarty->assign($original_smarty_vars);
--- app/views/shared/helpers/_button_create_new.tpl
+++ app/views/shared/helpers/_button_create_new.tpl
@@ -1,1 +1,1 @@
-<a href="{$create_new_url}" class="btn btn-default"><i class="glyphicon glyphicon-plus" title="{$title}"></i> <span class="hide">{$title}</span></a>
+<a href="{$create_new_url}"{foreach $attrs as $attr_k => $attr_v} {$attr_k}="{$attr_v}"{/foreach}><i class="glyphicon glyphicon-plus" title="{$title}"></i> <span class="hide">{$title}</span></a>
