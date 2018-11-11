c478078a95f3c16544b44b56cf2d94a48c42fc14
Jaromir Tomek <jaromir.tomek@gmail.com>
2018-11-11 19:48:45 +0100

    markdown-js removed from the project, seems to be unused

--- gulpfile-admin.js
+++ gulpfile-admin.js
@@ -13,7 +13,6 @@ var vendorScripts = [
 	"node_modules/jquery-ui-bundle/jquery-ui.js",
 	"node_modules/blueimp-file-upload/js/jquery.fileupload.js",
 	"bower_components/markdown-js/dist/markdown.js",
-	"bower_components/to-markdown/dist/to-markdown.js",
 	"node_modules/bootstrap-markdown/js/bootstrap-markdown.js",
 	"node_modules/bootstrap/dist/js/bootstrap.js",
 	"node_modules/atk14js/src/atk14.js"
