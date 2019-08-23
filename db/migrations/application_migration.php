cde895f3d4889aec382b346b69ef5c96dd5b58a3
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-08-23 15:31:37 +0200

    Added useful methods into ApplicationMigration: _link_to() and _link_to_page()

--- db/migrations/application_migration.php
+++ db/migrations/application_migration.php
@@ -17,4 +17,12 @@ class ApplicationMigration extends Atk14Migration{
 		return str_replace("\n",$connector,$lipsum);
 	}
 
+	function _link_to($params){
+		return Atk14Url::BuildLink($params);
+	}
+
+	function _link_to_page($code){
+		Atk14Require::Helper("modifier.link_to_page");
+		return smarty_modifier_link_to_page($code);
+	}
 }
