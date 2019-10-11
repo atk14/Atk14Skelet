06d514852b58e2529cd3127f45d2e1e28a5b1774
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-10-11 12:05:10 +0200

    Tests initialization tuned

--- test/models/initialize.php
+++ test/models/initialize.php
@@ -1,4 +1,6 @@
 <?php
 define("TEST",true);
+define("ATK14_DEFAULT_LANG","en");
 define("MY_BLOWFISH_ROUNDS",6); // default value 12 makes hashing very slow
+
 require(__DIR__."/../../atk14/load.php");
