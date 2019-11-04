a39fc9cb6058f23a71c29afaeb1d7ef20a7996cf
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-11-04 14:39:39 +0100

    Error redirections administration tuned

--- app/forms/admin/error_redirections/error_redirections_form.php
+++ app/forms/admin/error_redirections/error_redirections_form.php
@@ -4,20 +4,22 @@ class ErrorRedirectionsForm extends AdminForm {
 	function set_up(){
 		$this->add_field("source_url", new CharField(array(
 			"label" => _("Source URL"),
-			"hints" => array(
-				"http://example.com/document.pdf",
-				"//example.com/document.pdf",
-				"/document.pdf",
-			),
+			"help_text" => _("Address which cannot be found on this server. Here are some examples. The first one is usually the most appropriate.").
+				"<ul>".
+					"<li>/link-to-page/</li>".
+					"<li>//www.example.com/link-to-page/</li>".
+					"<li>http://www.example.com/link-to-page/</li>".
+				"</ul>",
 		)));
 
 		$this->add_field("target_url", new CharField(array(
 			"label" => _("Target URL"),
-			"hints" => array(
-				"http://example.com/public/document.pdf",
-				"//example.com/public/document.pdf",
-				"/public/document.pdf",
-			)
+			"help_text" => _("Examples:").
+				"<ul>".
+					"<li>/about-us/</li>".
+					"<li>//www.example.com/about-us/</li>".
+					"<li>http://www.example.com/about-us/</li>".
+				"</ul>",
 		)));
 
 		$this->add_field("moved_permanently", new BooleanField(array(
