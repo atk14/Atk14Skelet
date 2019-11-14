c9c355985f01adff74d84bffa70ec9a0261484a5
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-11-14 19:01:35 +0100

    Testing custom error404 page

--- app/models/trait_get_instance_by_code.php
+++ app/models/trait_get_instance_by_code.php
@@ -1,10 +1,14 @@
 <?php
 trait TraitGetInstanceByCode {
 	
-	static function GetInstanceByCode($code){
+	static function GetInstanceByCode($code,$options = array()){
 		static $instances;
 
-		if(!$instances){
+		$options += array(
+			"refresh_cache" => false,
+		);
+
+		if(!$instances || $options["refresh_cache"]){
 			$instances = [];
 			foreach(self::FindAll("code IS NOT NULL",[],["use_cache" => true]) as $c){
 				$instances[$c->getCode()] = $c;
