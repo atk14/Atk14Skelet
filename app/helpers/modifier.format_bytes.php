796eceabaf62e77f2f19e066cdce98a9b4b37282
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-12-08 16:14:53 +0100

    Modifier format_bytes improved & tested

--- app/helpers/modifier.format_bytes.php
+++ app/helpers/modifier.format_bytes.php
@@ -1,13 +1,18 @@
 <?php
 /**
-* {$bytes|format_bytes} --> 1,2 KB
-*/
+ *
+ *	{$bytes|format_bytes} --> 1,2 KB
+ */
 function smarty_modifier_format_bytes($bytes){
-	if($bytes>(1024*1024)){ return _format_bytes(($bytes/(1024 * 1024)),"MB"); }
-	if($bytes>1024){ return _format_bytes(($bytes/1024),"kB"); }
-	return "$bytes Bytes";
+	if(!strlen($bytes)){ return ""; }
+	if($bytes>=(1024*1024*1024)){ return _format_bytes(($bytes/(1024 * 1024 * 1024)),"GB"); }
+	if($bytes>=(1024*1024)){ return _format_bytes(($bytes/(1024 * 1024)),"MB"); }
+	if($bytes>=1024){ return _format_bytes(($bytes/1024),"kB"); }
+	$bytes = (int)$bytes;
+	return $bytes." Bytes";
 }
 
-function _format_bytes($mnozstvi,$jedotka){
-	return number_format($mnozstvi,1,",","")." $jedotka";
+function _format_bytes($mnozstvi,$jednotka){
+	$mnozstvi = number_format($mnozstvi,1,".","");
+	return Atk14Locale::FormatNumber($mnozstvi)." $jednotka";
 }
---
+++ test/helpers/tc_format_bytes.php
@@ -0,0 +1,18 @@
+<?php
+class TcFormatBytes extends TcBase {
+
+	function test(){
+		Atk14Require::Helper("modifier.format_bytes");
+
+		$this->assertEquals("",smarty_modifier_format_bytes(""));
+		$this->assertEquals("100 Bytes",smarty_modifier_format_bytes("100"));
+		$this->assertEquals("1.0 kB",smarty_modifier_format_bytes("1024"));
+		$this->assertEquals("2.2 kB",smarty_modifier_format_bytes("2222"));
+		$this->assertEquals("3.4 MB",smarty_modifier_format_bytes("3584707"));
+		$this->assertEquals("1.3 GB",smarty_modifier_format_bytes("1380674586"));
+
+		$locale = "cs";
+		Atk14Locale::Initialize($locale);
+		$this->assertEquals("1,3 GB",smarty_modifier_format_bytes("1380674586"));
+	}
+}
