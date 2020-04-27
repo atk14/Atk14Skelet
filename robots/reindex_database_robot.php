d38053527a237a033086a4e23913eef7bb0fce17
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-04-27 15:54:23 +0200

    Robot ReindexDatabaseRobot improved

--- robots/reindex_database_robot.php
+++ robots/reindex_database_robot.php
@@ -3,10 +3,42 @@
  * In the current database reindexes all tables and materialized views belonging to the currently connected user
  *
  * It may be useful to run it daily or weekly.
+ *
+ * To reindex all table in the current schema:
+ *
+ *	./scripts/robot_runner reindex_database
+ *	# or
+ *	./scripts/robot_runner --robot=reindex_database
+ *
+ * To reindex table with the specific name and next tables in alphabetical order:
+ *
+ *	./scripts/robot_runner --robot=reindex_database --start-with-table=users
+ *	# or
+ *	./scripts/robot_runner --robot=reindex_database -susers
  */
 class ReindexDatabaseRobot extends ApplicationRobot {
 
 	function run(){
+		$arguments = getopt("s::",array("start-with-table::"));
+		$arguments += array(
+			"s" => null,
+		);
+		$arguments += array(
+			"start-with-table" => $arguments["s"],
+		);
+
+		$schema = Atk14Migration::GetDatabaseSchema();
+		$bind_ar = array(
+			":schema" => $schema
+		);
+
+		$start_with_table = $arguments["start-with-table"];
+		$start_with_table_q = "";
+		if(strtolower($start_with_table)){
+			$start_with_table_q = " WHERE q.tablename>=:start_with_table";
+			$bind_ar[":start_with_table"] = "$schema.$start_with_table";
+		}
+
 		$tables = $this->dbmole->selectIntoArray("
 			SELECT tablename FROM (
 				SELECT
@@ -15,7 +47,8 @@ class ReindexDatabaseRobot extends ApplicationRobot {
 					pg_tables
 				WHERE
 					hasindexes AND
-					tableowner=(SELECT CURRENT_USER)
+					tableowner=(SELECT CURRENT_USER) AND
+					schemaname=:schema
 				UNION
 				SELECT
 					schemaname||'.'||matviewname AS tablename
@@ -24,8 +57,8 @@ class ReindexDatabaseRobot extends ApplicationRobot {
 				WHERE
 					hasindexes AND
 					matviewowner=(SELECT CURRENT_USER)
-			)q ORDER BY tablename
-		");
+			)q$start_with_table_q ORDER BY tablename
+		",$bind_ar);
 		foreach($tables as $table){
 			$this->dbmole->doQuery("REINDEX TABLE $table");
 			$this->logger->info("table $table reindexed");
