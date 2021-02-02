df6b81ad5ee25716020bd4886a09dbe54987c78f
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-02-02 11:10:22 +0100

    [TemporaryFileUpload + AsyncFileField] The proper dropZone specified on AsyncFileField

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -114,6 +114,7 @@
 						maxChunkSize: 1024 * 1024,
 						multipart: false,
 						sequentialUploads: true,
+						dropZone: $(this),
 						start: function() {
 							this.$wrap = $( this ).parents( ".js--async-file" );
 							this.$wrap.html( this.$wrap.data( "template_loading" ) );
