c31c58bca53136b9f50d57faef2187790c392d26
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-03-08 10:26:54 +0100

    Controller testing fixed

--- test/controllers/admin/tc_base.php
+++ test/controllers/admin/tc_base.php
@@ -3,7 +3,7 @@
  * The base class of every controller`s test case class.
  *
  * Notice that TcBase is descendant of TcAtk14Controller
- * so there is a couple of special member variables in advance.
+ * so there are a couple of special member variables in advance.
  */
 class TcBase extends TcAtk14Controller{
 
--- test/controllers/api/tc_base.php
+++ test/controllers/api/tc_base.php
@@ -3,12 +3,13 @@
  * The base class of every controller`s test case class.
  *
  * Notice that TcBase is descendant of TcAtk14Controller
- * so there is a couple of special member variables in advance.
+ * so there are a couple of special member variables in advance.
  */
 class TcBase extends TcAtk14Controller{
 
 	function setUp(){
 		$this->dbmole->begin();
+		$this->setUpFixtures();
 		$this->client->setRemoteAddr("127.0.0.1");
 	}
 
