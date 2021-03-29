ef6a4b83bb21f3216ec8ffc50012663081eedfd3
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-03-29 11:46:50 +0200

    TraitTags improved; methods setTag(), addTag() and containsTag() are accepting Tag, id or code

--- app/models/trait_tags.php
+++ app/models/trait_tags.php
@@ -13,10 +13,12 @@ trait TraitTags {
 	}
 
 	function setTags($tags){
+		$tags = $this->_cleanTag($tags);
 		return $this->getTagsLister()->setRecords($tags);
 	}
 
 	function addTag($tag){
+		$tag = $this->_cleanTag($tag);
 		return $this->getTagsLister()->add($tag);
 	}
 
@@ -26,7 +28,31 @@ trait TraitTags {
 		}
 	}
 
+	/**
+	 *
+	 *	$article->containsTag($tag);
+	 *	$article->containsTag($code); // e.g. "news"
+	 *	$article->containsTag($id); // e.g. 123
+	 */
 	function containsTag($tag){
+		$tag = $this->_cleanTag($tag);
 		return $this->getTagsLister()->contains($tag);
+	}
+
+	private function _cleanTag($tag){
+		if(is_array($tag)){
+			$out = array();
+			foreach($tag as $k => $t){
+				$out[$k] = $this->_cleanTag($t);
+			}
+			return $out;
+		}
+
+		if(is_numeric($tag) && ($t = Cache::Get("Tag",$tag))){
+			$tag = $t;
+		}elseif(!is_a($tag,"Tag") && ($t = Tag::GetInstanceByCode("$tag"))){
+			$tag = $t;
+		}
+		return $tag;
 	}
 }
--- test/models/tc_trait_tags.php
+++ test/models/tc_trait_tags.php
@@ -10,6 +10,8 @@ class TcTraitTags extends TcBase {
 		$article = $this->articles["testing_article"];
 		$fun = $this->tags["fun"];
 		$music = $this->tags["music"];
+		$spring = $this->tags["spring"];
+		$spring_id = $this->tags["spring"]->getId();
 
 		$this->assertEquals(array(),$article->getTags());
 		$this->assertEquals(false,$article->containsTag($fun));
@@ -26,5 +28,27 @@ class TcTraitTags extends TcBase {
 		$this->assertEquals(array($fun,$music),$tags);
 		$this->assertEquals(true,$article->containsTag($fun));
 		$this->assertEquals(true,$article->containsTag($music));
+
+		// --
+
+		$this->assertEquals(false,$article->containsTag($spring));
+		$this->assertEquals(false,$article->containsTag("spring 2000"));
+		$this->assertEquals(false,$article->containsTag("spring_2000"));
+		$this->assertEquals(false,$article->containsTag($spring->getId()));
+
+		$article->addTag($spring);
+
+		$this->assertEquals(true,$article->containsTag($spring));
+		$this->assertEquals(false,$article->containsTag("spring 2000")); // !! false; there is no tag with code == "spring 2000"
+		$this->assertEquals(true,$article->containsTag("spring_2000"));
+		$this->assertEquals(true,$article->containsTag($spring->getId()));
+
+		// --
+
+		$article->setTags(array());
+		$this->assertEquals(array(),$article->getTags());
+
+		$article->setTags(array($fun,$music->getId(),"spring_2000"));
+		$tags = $this->assertEquals(array($fun,$music,$spring),$article->getTags());
 	}
 }
