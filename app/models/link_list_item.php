8b00c34a2a6cbc3708e395d72b52129053ca4f25
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-07-01 10:58:34 +0200

    Added method LinkListItem::getSubmenu()

--- app/models/link_list_item.php
+++ app/models/link_list_item.php
@@ -18,4 +18,63 @@ class LinkListItem extends ApplicationModel implements Rankable, Translatable {
 	}
 
 	function isVisible(){ return $this->g("visible"); }
+
+	/**
+	 * Tries to determine the object this link is pointing to
+	 *
+	 *	$target = $item->getTargetObject();
+	 *	if($target && is_a($target,"Page")){
+	 *		// well, $target is a page :)
+	 *  }
+	 */
+	function getTargetObject(){
+		$params = $this->g("url_params");
+		if($params){ $params = json_decode($params,true); }
+		if(!$params){ return; }
+
+		if($params["namespace"]!==""){ return; }
+		switch("$params[controller]/$params[action]"){
+			case "pages/detail":
+				return Cache::Get("Page",(int)$params["id"]);
+			case "main/index":
+				return Page::GetInstanceByCode("homepage");
+		}
+	}
+
+	/**
+	 * Returns auto generated submenu
+	 *
+	 * Returns null when there is no submenu for this item.
+	 *
+	 * @return Menu14
+	 */
+	function getSubmenu($options = array()){
+		$options += array(
+			"reasonable_max_items_count" => null, // null will be returned when the count of submenu items exceeds this value
+		);
+
+		$params = $this->g("url_params");
+		if($params){ $params = json_decode($params,true); }
+		$target = $this->getTargetObject();
+
+		$menu = new Menu14();
+
+		if(is_a($target,"Page")){
+			$menu->setMeta("image_url",$target->getImageUrl());
+			foreach($target->getVisibleChildPages() as $chi){
+				$item = $menu->addItem($chi->getTitle(),Atk14Url::BuildLink(["namespace" => "", "action" => "pages/detail", "id" => $chi]));
+				$item->setMeta("image_url",$chi->getImageUrl());
+			}
+		}
+
+		if($menu->isEmpty()){
+			return null;
+		}
+
+		if($options["reasonable_max_items_count"] && sizeof($menu->getItems())>$options["reasonable_max_items_count"]){
+			return null;
+		}
+
+		return $menu;
+	}
 }
---
+++ test/models/tc_link_list_item.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * @fixture link_list_items
+ * @fixture pages
+ */
+class TcLinkListItem extends TcBase {
+
+	function test(){
+		$li_testing_page = $this->link_list_items["main_menu__testing_page"];
+		$target = $li_testing_page->getTargetObject();
+		$this->assertEquals(true,is_a($target,"Page"));
+		$this->assertEquals("testing_page",$target->getCode());
+
+		$li_homepage = $this->link_list_items["main_menu__homepage"];
+		$target = $li_homepage->getTargetObject();
+		$this->assertEquals(true,is_a($target,"Page"));
+		$this->assertEquals("homepage",$target->getCode());
+
+		$li_external = $this->link_list_items["main_menu__external"];
+		$this->assertEquals(null,$li_external->getTargetObject());
+	}
+
+	function test_getSubmenu(){
+		$lli = $this->link_list_items["main_menu__testing_page"];
+		$submenu = $lli->getSubmenu();
+		$this->assertNotNull($submenu);
+		$items = $submenu->getItems();
+		$this->assertEquals(1,sizeof($items)); // there is one subpage
+		$this->assertEquals(Atk14Url::BuildLink(["namespace" => "", "action" => "pages/detail", "id" => $this->pages["testing_subpage"]]),$items[0]->getUrl());
+
+		$lli = $this->link_list_items["main_menu__external"];
+		$this->assertEquals(null,$lli->getSubmenu());
+	}
+}
