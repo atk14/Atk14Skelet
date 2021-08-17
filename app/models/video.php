c03716f66fb53b2bb48787b1639399baa8eb7f13
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-08-17 14:09:37 +0200

    Lazy load embedded videos in iObjects

--- app/models/video.php
+++ app/models/video.php
@@ -58,6 +58,29 @@ HTML;
 			$out = preg_replace('/(src=".*?)"/','\1&amp;loop=1&amp;playlist='.$video_id.'"',$out);
 		}
 
+		// https://css-tricks.com/lazy-load-embedded-youtube-videos/
+		if(!$this->g("autoplay") && !preg_match('/srcdoc=/') && preg_match('/src="(.*?)"/',$out,$matches)){
+			$_url = $matches[1];
+			$_url = $_url . (preg_match('/\?/',$_url) ? "&amp;autoplay=1" : "?autoplay=1");
+			$p = new Pupiq($this->getImageUrl());
+			$_image_url = $p->getUrl("800x800");
+			$srcdoc = array();
+			$srcdoc[] = "<style>";
+			$srcdoc[] = "*{padding:0;margin:0;overflow:hidden}html,body{height:100%}";
+			$srcdoc[] = "img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}";
+			//$srcdoc[] = "img{filter: blur(3px)}";
+			$srcdoc[] = "span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}";
+			$srcdoc[] = "</style>";
+			//
+			$srcdoc[] = "<a href=".$_url.">";
+			$srcdoc[] = "<img src=".$_image_url." alt='".h($this->getTitle())."'>";
+			$srcdoc[] = "<span>▶</span>";
+			$srcdoc[] = "</a>";
+			$srcdoc = join("",$srcdoc);
+
+			$out = preg_replace('/src=".*?"/','\1 srcdoc="'.h($srcdoc).'"',$out);
+		}
+
 		return $out;
 	}
 
