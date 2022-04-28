016c1cf414c11f159e4223ee55e0a46a8512c370
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-04-28 16:06:22 +0200

    NewsletterSubscriber improved

--- app/models/newsletter_subscriber.php
+++ app/models/newsletter_subscriber.php
@@ -1,6 +1,39 @@
 <?php
 class NewsletterSubscriber extends ApplicationModel{
 
+	static function GetInstancesByEmail($email){
+		$query = "
+			SELECT id FROM (
+				SELECT
+					id,created_at
+				FROM
+					newsletter_subscribers
+				WHERE
+					LOWER(email)=LOWER(:email)
+				--	UNION
+				--	SELECT
+				--		newsletter_subscribers.id,newsletter_subscribers.created_at
+				--	FROM
+				--		users,
+				--		newsletter_subscribers
+				--	WHERE
+				--		LOWER(users.email)=LOWER(:email) AND
+				--		newsletter_subscribers.user_id=users.id
+			)q ORDER BY created_at DESC, id DESC
+		";
+
+		$dbmole = self::GetDbmole();
+		$ids = $dbmole->selectIntoArray($query,[":email" => $email]);
+
+		return Cache::Get("NewsletterSubscriber",$ids);
+	}
+
+	static function GetMostRecentInstanceByEmail($email,$options = []){
+		if($ns_ar = self::GetInstancesByEmail($email)){
+			return $ns_ar[0];
+		}
+	}
+
 	static function CreateNewRecord($values,$options = []){
 		global $ATK14_GLOBAL;
 		
@@ -20,12 +53,21 @@ class NewsletterSubscriber extends ApplicationModel{
 	 * NewsletterSubscriber::SignUp("john@doe",array("name" => "John Doe"));
 	 * NewsletterSubscriber::SignUp("john@doe",array("vocative" => "Mr.", "name" => "John Doe"));
 	 */
-	static function SignUp($user_or_email,$values = array()){
-		global $ATK14_GLOBAL;
+	static function SignUp($user_or_email,$values = array(),&$subscription_just_created = false,$options = array()){
+		global $ATK14_GLOBAL, $HTTP_REQUEST;
 
 		$values += [
 			"language" => $ATK14_GLOBAL->getLang(),
 		];
+
+		$subscription_just_created = false;
+
+		$options += array(
+			"request" => $HTTP_REQUEST
+		);
+
+
+		$request = $options["request"];
 
 		$values_create = array();
 		if(is_a($user_or_email,"User")){
@@ -45,12 +87,16 @@ class NewsletterSubscriber extends ApplicationModel{
 				}
 			}
 			if($upd_array){
-				$upd_array["updated_from_addr"] = $GLOBALS["HTTP_REQUEST"]->getRemoteAddr();
+				$upd_array["updated_from_addr"] = $request->getRemoteAddr();
 				$ns->s($upd_array);
 			}
 		}else{
-			$values["created_from_addr"] = $GLOBALS["HTTP_REQUEST"]->getRemoteAddr();
+			$values += [
+				"created_from_addr" => $request->getRemoteAddr(),
+				"subscribed_at_url" => $request->getUrl(),
+			];
 			$ns = NewsletterSubscriber::CreateNewRecord($values + $values_create);
+			$subscription_just_created = true;
 		}
 
 		return $ns;
