a876cdfc0d10504d5b5ffa2559a6c3a20d42e388
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-06-27 13:09:40 +0200

    ApplicationRobot prepared for testing

--- robots/application_robot.php
+++ robots/application_robot.php
@@ -3,13 +3,16 @@
  * The base class for every other robot.
  */
 class ApplicationRobot extends Atk14Robot{
+
 	function beforeRun(){
+		if(TEST){ return; }
 		$this->dbmole->begin(array(
 			"execute_after_connecting" => true
 		));
 	}
 
 	function afterRun(){
+		if(TEST){ return; }
 		$this->dbmole->commit();
 	}
 }
