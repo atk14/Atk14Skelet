b033e84353f3b19689405f7c64bc53f6d4adab90
Jaromir Tomek <jaromir.tomek@gmail.com>
2023-03-02 14:50:18 +0100

    Block helpers highlight_search_query improved, added modifier highlight_search_query

--- app/helpers/block.highlight_search_query.php
+++ app/helpers/block.highlight_search_query.php
@@ -9,12 +9,19 @@
  *	{/highlight_search_query}
  */
 function smarty_block_highlight_search_query($params,$content,$template,&$repeat){
+	if($repeat){ return; }
 
 	Atk14Require::Helper("block.highlight_keywords");
 
+	$params += [
+		"param_name" => "search",
+	];
+	$param_name = $params["param_name"];
+	unset($params["param_name"]);
+
 	$_params = $template->getTemplateVars("params");
 	if($_params && is_a($_params,"Dictionary")){
-		$params["keywords"] = $_params->getString("search");
+		$params["keywords"] = $_params->getString($param_name);
 	}
 
 	return smarty_block_highlight_keywords($params,$content,$template,$repeat);
