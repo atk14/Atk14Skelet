a931aed206f21c244a46ec552ecb51c9909f0118
Jakub Tylčer <tylcer@snapps.eu>
2023-06-05 14:15:00 +0200

    CSS compiles with success in Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,20 @@ const path = require("path");
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const webpack = require('webpack');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-var vendorScripts = [
+/*var vendorScripts = [
 	"./node_modules/jquery/dist/jquery.js",
 	"./node_modules/bootstrap/dist/js/bootstrap.bundle.js", // Bootstrap + Popper
 	"./node_modules/atk14js/src/atk14.js",
 	"./node_modules/unobfuscatejs/src/jquery.unobfuscate.js",
 	"./node_modules/swiper/swiper-bundle.js"
-];
+];*/
 
 var applicationScripts = [
 	"./public/scripts/utils/utils.js",
-	//"./public/scripts/utils/swiper.js",
 	"./public/scripts/application.js",
   
 ];
@@ -22,6 +24,7 @@ module.exports = {
   entry: {
     application: applicationScripts,
     application_es6: "./public/scripts/modules/application_es6.js",
+    styles: "./public/styles/application.scss",
     //vendor: vendorScripts,
     //test: "./public/test/test.js",
     //vendor: "./public/test/vendor.js"
@@ -49,9 +52,19 @@ module.exports = {
     ),
     new FaviconsWebpackPlugin( {
       logo: "./public/favicons/favicon.png",
-      prefix: "favicons/",
-      inject: false
-    } )
+      //prefix: "favicons/",
+      outputPath: '../favicons',
+      inject: false,
+      favicons: {
+        icons : {
+          yandex: false,
+          appleStartup: false
+        }
+      }
+    } ),
+    new MiniCssExtractPlugin(),
+    require ('autoprefixer'),
+    new MiniCssExtractPlugin(),
   ],
   module: { 
     "rules": [ 
@@ -68,7 +81,59 @@ module.exports = {
             presets: [ "@babel/preset-env", ] 
           } 
         } 
-      }, 
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            /*options: {
+              plugins: function () { // post css plugins, can be exported to postcss.config.js
+                return [
+                  require('precss'),
+                  require('autoprefixer')
+                ];
+              }
+            }*/
+          },
+          "sass-loader",
+        ],
+      },
+      /*{
+        test: /\.(scss)$/,
+        //test: /\.s[ac]ss$/i,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            plugins: function () { // post css plugins, can be exported to postcss.config.js
+              return [
+                require('precss'),
+                require('autoprefixer')
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },*/
+      /*{
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+            {
+                loader: 'file-loader',
+                options: { outputPath: 'css/', name: '[name].min.css'}
+            },
+            'sass-loader'
+        ]
+      }*/
+
     ] 
   },
   devtool: false,
@@ -83,5 +148,11 @@ module.exports = {
         }
       },
     },
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
+    ],
+    minimize: true
   }
 };
