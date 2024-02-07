7a6a7832fdccebb3bfe34f4c8c071c9a67b91af1
Jakub Tylčer <tylcer@snapps.eu>
2024-02-07 15:53:56 +0100

    Suggestions css + js files moved

--- gulpfile-admin.js
+++ gulpfile-admin.js
@@ -29,7 +29,7 @@ var applicationScripts = [
 	"public/scripts/utils/utils.js",
 	"public/scripts/utils/leaving_unsaved_page_checker.js",
 	"public/scripts/utils/async_file_upload.js",
-	"public/admin/scripts/utils/suggestions.js",
+	"public/scripts/utils/suggestions.js",
 	"public/admin/scripts/application.js",
 ];
 
