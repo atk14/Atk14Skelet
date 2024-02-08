b0aaa27665d7e5e5a15534e0c37e9cb409c2a67b
Jakub Tylčer <tylcer@snapps.eu>
2024-02-08 14:39:58 +0100

    TagChooser enhancements

--- public/admin/scripts/utils/tag_chooser.js
+++ public/admin/scripts/utils/tag_chooser.js
@@ -1,3 +1,13 @@
+/* Tag chooser
+ * Nice graphical UI for tag selection input
+ * appends itself to #id_tag input
+ * 
+ * TODO: optional autocomplete integration via window.UTILS.Suggestions
+ *       optional Drag + Drop rearrange
+ * 
+ * Usage: window.UTILS.TagChooser.init();
+ * 
+*/
 window.UTILS = window.UTILS || { };
 
 window.UTILS.TagChooser = class {
@@ -11,7 +21,7 @@ window.UTILS.TagChooser = class {
 
   static init() {
     console.log( "Hi, I am your brand new TagChooser!" );
-    let inputs = document.querySelectorAll( "[data-tags_suggesting='yes']" );
+    let inputs = document.querySelectorAll( "#id_tags" );
     let lang = document.querySelector( "html" ).getAttribute( "lang" );
     this.url = "/api/" + lang + "/tags_suggestions/?format=json&q=";
     //[...inputs].forEach( this.createTagChooser.bind( this ) );
@@ -29,6 +39,7 @@ window.UTILS.TagChooser = class {
     console.log( this.split( "a,b b, c, dede") );
     this.input.style.opacity = 0.7;
     this.wrapper = document.createElement( "div" );
+    this.wrapper.classList.add( "tag-chooser" );
     this.singleTagInput = document.createElement( "input" );
     this.singleTagInput.setAttribute( "id", "tag_chooser_input_" + this.numInstances );
     this.singleTagInput.setAttribute( "type", "text" );
@@ -44,8 +55,9 @@ window.UTILS.TagChooser = class {
   
   }
 
+  // create new tag item from single tag input value on Enter
   addTag( e ) {
-    if (e.key === 'Enter' || e.keyCode === 13) {
+    if ( e.key === 'Enter' || e.keyCode === 13 ) {
       e.preventDefault();
       console.log( e.target.value );
       if( e.target.value.length > 0 )
@@ -54,6 +66,7 @@ window.UTILS.TagChooser = class {
     }
   }
 
+  // remove tag item
   removeTag( e ) {
     console.log( "removeTag", e.target.closest( ".badge" ) );
     e.target.closest( ".badge" ).remove();
@@ -62,19 +75,29 @@ window.UTILS.TagChooser = class {
 
   renderTag( tagName ) {
     tagName = tagName.trim();
+
+    // check to prevent whitespace only 
     if( tagName.length < 1 ) {
       return;
     }
+
+    // check to prevent duplicate tags
+    if ( this.wrapper.querySelector( ".tag-item[data-tag_name='" + tagName + "']") ) {
+      return
+    };
+
+    // create tag item
     let tagItem = document.createElement( "div" );
     tagItem.classList.add( "badge" );
     tagItem.classList.add( "badge-info" );
     tagItem.classList.add( "badge-pill" );
+    tagItem.classList.add( "tag-item" );
     tagItem.style.fontSize = "1.25rem";
     let removeBtn = document.createElement( "div" );
     removeBtn.classList.add( "d-inline-flex" );
     removeBtn.classList.add( "remove_btn" );
-    removeBtn.classList.add( "px-1" );
-    removeBtn.append( "x" );
+    removeBtn.classList.add( "pr-1" );
+    removeBtn.insertAdjacentHTML( "afterbegin", "<i class=\"fa-solid fa-circle-xmark\"></i>" );
     removeBtn.addEventListener( "click", this.removeTag.bind( this ) );
     tagItem.appendChild( removeBtn );
     tagItem.append( tagName );
@@ -85,8 +108,9 @@ window.UTILS.TagChooser = class {
     this.onChange();
   }
 
+  // When tags change write their comma separated list to original input
   onChange(){
-    let tagItems = this.wrapper.querySelectorAll( "[data-tag_name]" );
+    let tagItems = this.wrapper.querySelectorAll( ".tag-item[data-tag_name]" );
     let tagList = "";
     [...tagItems].forEach( ( item ) => {
       tagList += item.dataset.tag_name + ", ";
@@ -98,7 +122,8 @@ window.UTILS.TagChooser = class {
   split( val ) {
     return val.split( /,\s*/ );
   }
-  extractLast( t ) {
+
+  /*extractLast( t ) {
     return this.split( t ).pop();
-  }
+  }*/
 };
