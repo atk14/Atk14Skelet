9bd52cb8362df2bce3761f756439268544759be7
Jakub Tylčer <tylcer@snapps.eu>
2024-02-09 14:41:04 +0100

    TagChooser tuning

--- public/admin/scripts/utils/tag_chooser.js
+++ public/admin/scripts/utils/tag_chooser.js
@@ -17,6 +17,20 @@ window.UTILS.TagChooser = class {
   parent;
   wrapper;
   singleTagInput;
+  addTagBtn;
+  tagContainer;
+  template = `
+  <div class="tag_chooser js--tag_chooser">
+    <div class="input-group">
+      <input type="text" class="form-control js--single_tag_input" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary js--single_tag_btn" type="button" id="button-addon2"><i class=\"fa-solid fa-circle-plus\"></i></button>
+      </div>
+    </div>
+    <div class="tag_container js--tag_container"></div>
+  </div>
+  `;
+  
 
 
   static init() {
@@ -35,34 +49,38 @@ window.UTILS.TagChooser = class {
     console.log( "createTagChooser", input );
     this.parent = this.input.parentElement;
     this.parent.style.outline = "1px solid red";
-    console.log( this );
-    console.log( this.split( "a,b b, c, dede") );
     this.input.style.opacity = 0.7;
-    this.wrapper = document.createElement( "div" );
-    this.wrapper.classList.add( "tag-chooser" );
-    this.singleTagInput = document.createElement( "input" );
-    this.singleTagInput.setAttribute( "id", "tag_chooser_input_" + this.numInstances );
-    this.singleTagInput.setAttribute( "type", "text" );
-    this.singleTagInput.classList.add( "form-control" );
-    this.singleTagInput.classList.add( "text" );
-    this.wrapper.appendChild( this.singleTagInput );
-    this.parent.appendChild( this.wrapper );
+    
+    this.parent.insertAdjacentHTML( "beforeend", this.template );
+
+    this.wrapper = this.parent.querySelector( ".js--tag_chooser" ); console.log( "test", this.wrapper );
+    this.singleTagInput = this.parent.querySelector( ".js--single_tag_input" );
+    this.addTagBtn = this.parent.querySelector( ".js--single_tag_btn" );
+    this.tagContainer = this.parent.querySelector( ".js--tag_container" );
 
     let tags = this.split( this.input.value );
     tags.forEach( this.renderTag.bind( this ) );
 
-    this.singleTagInput.addEventListener( "keydown", this.addTag.bind( this ) );
-  
+    this.singleTagInput.addEventListener( "keydown", this.onInputEnter.bind( this ) );
+    this.addTagBtn.addEventListener( "click", this.onBtnClick.bind( this ) );
   }
 
   // create new tag item from single tag input value on Enter
-  addTag( e ) {
-    if ( e.key === 'Enter' || e.keyCode === 13 ) {
+  onInputEnter( e ) {
+    if ( e.key === "Enter" || e.keyCode === 13 ) {
       e.preventDefault();
       console.log( e.target.value );
-      if( e.target.value.length > 0 )
-      this.renderTag( e.target.value );
-      e.target.value = "";
+      if( e.target.value.length > 0 ){
+        this.renderTag( e.target.value );
+        e.target.value = "";
+      }
+    }
+  }
+
+  onBtnClick( e ) {
+    e.preventDefault();
+    if( this.singleTagInput.value.length > 0 ) {
+      this.renderTag( this.singleTagInput.value );
     }
   }
 
@@ -73,44 +91,40 @@ window.UTILS.TagChooser = class {
     this.onChange();
   }
 
-  renderTag( tagName ) {
-    tagName = tagName.trim();
+  renderTag( input ) {
+    // split input by commas in case there are more than one tag entered at once
+    let tagNames = this.split( input );
 
-    // check to prevent whitespace only 
-    if( tagName.length < 1 ) {
-      return;
-    }
+    // render tag items
+    tagNames.forEach( ( tagName )=>{
+      tagName = tagName.trim();
 
-    // check to prevent duplicate tags
-    if ( this.wrapper.querySelector( ".tag-item[data-tag_name='" + tagName + "']") ) {
-      return
-    };
+      // check to prevent whitespace only 
+      if( tagName.length < 1 ) {
+        return;
+      }
 
-    // create tag item
-    let tagItem = document.createElement( "div" );
-    tagItem.classList.add( "badge" );
-    tagItem.classList.add( "badge-info" );
-    tagItem.classList.add( "badge-pill" );
-    tagItem.classList.add( "tag-item" );
-    tagItem.style.fontSize = "1.25rem";
-    let removeBtn = document.createElement( "div" );
-    removeBtn.classList.add( "d-inline-flex" );
-    removeBtn.classList.add( "remove_btn" );
-    removeBtn.classList.add( "pr-1" );
-    removeBtn.insertAdjacentHTML( "afterbegin", "<i class=\"fa-solid fa-circle-xmark\"></i>" );
-    removeBtn.addEventListener( "click", this.removeTag.bind( this ) );
-    tagItem.appendChild( removeBtn );
-    tagItem.append( tagName );
-    //tagItem.innerHtml = removeBtn + tagName;
-    tagItem.dataset.tag_name = tagName;
-    this.wrapper.appendChild( tagItem );
-    
+      // check to prevent duplicate tags
+      if ( this.wrapper.querySelector( ".tag-item[data-tag_name='" + tagName + "']") ) {
+        return
+      };
+
+      let tagItemTemplate = `
+      <div class="badge badge-pill badge-info tag_item js--tag_item" data-tag_name="${tagName}">
+        <div class="remove_btn js--remove_btn"><i class="fa-solid fa-circle-xmark"></i></div>
+        ${tagName}
+      </div>
+    `;
+      
+      this.tagContainer.insertAdjacentHTML( "beforeend", tagItemTemplate );
+      this.tagContainer.querySelector( ".tag_item[data-tag_name='" + tagName+ "'] .js--remove_btn" ).addEventListener( "click", this.removeTag.bind( this ) );
+    })
     this.onChange();
   }
 
   // When tags change write their comma separated list to original input
   onChange(){
-    let tagItems = this.wrapper.querySelectorAll( ".tag-item[data-tag_name]" );
+    let tagItems = this.tagContainer.querySelectorAll( ".tag_item[data-tag_name]" );
     let tagList = "";
     [...tagItems].forEach( ( item ) => {
       tagList += item.dataset.tag_name + ", ";
