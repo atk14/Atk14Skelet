35fa9d1da903ce9c26e4b5e943f79423b34d90d7
Jakub Tylčer <tylcer@snapps.eu>
2024-02-20 15:04:13 +0100

    js debug

--- webpack.admin.config.js
+++ webpack.admin.config.js
@@ -17,6 +17,7 @@ var application_scripts = [
   "./public/scripts/utils/utils.js",
 	"./public/scripts/utils/leaving_unsaved_page_checker.js",
 	"./public/scripts/utils/async_file_upload.js",
+	"./public/scripts/utils/suggestions.js",
 	"./public/admin/scripts/utils/async_image_upload.js",
 	"./public/admin/scripts/application.js",
 ];
