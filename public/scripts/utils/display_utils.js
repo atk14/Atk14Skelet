5d0c11b3e5a07a476063141d3f43a1cb9832871a
Jakub Tylčer <tylcer@snapps.eu>
2024-03-01 15:01:15 +0100

    Rewriteen JS display_utils

--- public/scripts/utils/display_utils.js
+++ public/scripts/utils/display_utils.js
@@ -1,20 +1,25 @@
 
 /**
  * Replacement methods for some widely used jQuery methods
- * They extend HTMLElement class
  * 
  * Usage:
- * document.querySelector( "p.test" ).fadeOut();
+ * window.UTILS.fadeOut( document.querySelector( "p.test" ) );
+ * window.UTILS.fadeOut( document.querySelector( "p.test" ), "slow" );
+ * window.UTILS.fadeOut( document.querySelector( "p.test" ), "fast" );
+ * window.UTILS.fadeOut( document.querySelector( "p.test" ), 1500 );
  * 
  * fadeOut, fadeIn timing (same as in jQuery): <milliseconds>|"slow"|"fast"; default: 400ms, slow: 600ms, fast: 200ms (same as in jQuery) 
  * 
  */
 
+window.UTILS = window.UTILS || { };
+
 
 // Fade out - replacement to jQuery.fadeOut();
 
-HTMLElement.prototype.fadeOut = function( t ) {
-
+window.UTILS.fadeOut = function( el, t ) {
+	console.log( "fadeOut" );
+	console.trace();
 	// timing
 	if ( t === "slow" ) {
 		t = 600;
@@ -25,26 +30,26 @@ HTMLElement.prototype.fadeOut = function( t ) {
 	}
 
 	// remember element`s display property - to be restored by elemet.fadeIn()
-	if( window.getComputedStyle( this ).display !== "none" ) {
-		this.dataset.display_before_fade = window.getComputedStyle( this ).display;
+	if( window.getComputedStyle( el ).display !== "none" ) {
+		el.dataset.display_before_fade = window.getComputedStyle( el ).display;
 	}
 
 	// fade out
-	let fadeOutAnimation = this.animate( { opacity: 0}, { duration: t } );
+	let fadeOutAnimation = el.animate( { opacity: 0}, { duration: t } );
 
 	// set display:none after fade complete
 	fadeOutAnimation.addEventListener( "finish", () => {
-		this.style.display = "none";
-		this.style.opacity = 0;
+		el.style.display = "none";
+		el.style.opacity = 0;
 	} );
 };
 
 // Fade in - replacement to jQuery.fadeIn(); if element was hidden by element.fadeOut(), its original display mode will be restored, otherwise its display property will be set to "block".
 
-HTMLElement.prototype.fadeIn = function( t ) {
+window.UTILS.fadeIn = function( el, t ) {
 
 	// get current style
-	let currentStyle = window.getComputedStyle( this );
+	let currentStyle = window.getComputedStyle( el );
 	
 	// timing 
 	if ( t === "slow" ) {
@@ -58,23 +63,23 @@ HTMLElement.prototype.fadeIn = function( t ) {
 	// set appropriate display property
 	if( currentStyle.display === "none" ) {
 		// if element remembers it display property it had before element.fadeOut(), restore it, otherwise set it to "block"
-		if( this.dataset.display_before_fade !== "none" ){
-			this.style.display = this.dataset.display_before_fade;
+		if( el.dataset.display_before_fade !== "none" ){
+			el.style.display = el.dataset.display_before_fade;
 		} else {
-			this.style.display = "block";
+			el.style.display = "block";
 		}
 	}
 
 	// fade in 
 	if( currentStyle.opacity < 1 ){
-		let fadeInAnimation = this.animate( [ { opacity: 0}, { opacity: 1} ], { duration: t } );
+		let fadeInAnimation = el.animate( [ { opacity: 0}, { opacity: 1} ], { duration: t } );
 		fadeInAnimation.addEventListener( "finish", () => {
-			this.style.opacity = 1;
+			el.style.opacity = 1;
 			// restore original overflow if stored, otherwise set it to "auto"
-			if( this.dataset.overflowy_before_fade ) {
-				this.style.overflowY = this.dataset.overflowy_before_fade;
+			if( el.dataset.overflowy_before_fade ) {
+				el.style.overflowY = el.dataset.overflowy_before_fade;
 			} else {
-				this.style.overflowY = "auto";
+				el.style.overflowY = "auto";
 			}
 		} );
 	}
@@ -82,8 +87,9 @@ HTMLElement.prototype.fadeIn = function( t ) {
 
 // Hide - replacement to jQuery.fadeOut();
 
-HTMLElement.prototype.hide = function( t ) {
-
+window.UTILS.hide = function( el, t ) {
+	console.log( "hide" );
+	console.trace();
 	// timing
 	if ( t === "slow" ) {
 		t = 600;
@@ -94,28 +100,28 @@ HTMLElement.prototype.hide = function( t ) {
 	}
 
 	// remember element`s display property - to be restored by elemet.fadeIn()
-	if( window.getComputedStyle( this ).display !== "none" ) {
-		this.dataset.display_before_fade = window.getComputedStyle( this ).display;
+	if( window.getComputedStyle( el ).display !== "none" ) {
+		el.dataset.display_before_fade = window.getComputedStyle( el ).display;
 	}
-	this.dataset.overflowy_before_fade = window.getComputedStyle( this ).overflowY;
+	el.dataset.overflowy_before_fade = window.getComputedStyle( el ).overflowY;
 
 	// fade out, height to 0
-	this.style.overflowY = "hidden";
-	let fadeOutAnimation = this.animate( { opacity: 0, height: 0}, { duration: t } );
+	el.style.overflowY = "hidden";
+	let fadeOutAnimation = el.animate( { opacity: 0, height: 0}, { duration: t } );
 
 	// set display:none after fade complete
 	fadeOutAnimation.addEventListener( "finish", () => {
-		this.style.display = "none";
-		this.style.opacity = 0;
+		el.style.display = "none";
+		el.style.opacity = 0;
 	} );
 };
 
 // Show - replacement to jQuery.show(); if element was hidden by element.fadeOut() or element.hide, its original display mode will be restored, otherwise its display property will be set to "block".
 
-HTMLElement.prototype.show = function( t ) {
+window.UTILS.show = function( el, t ) {
 
 	// get current style
-	let currentStyle = window.getComputedStyle( this );
+	let currentStyle = window.getComputedStyle( el );
 	
 	// timing 
 	if ( t === "slow" ) {
@@ -129,23 +135,23 @@ HTMLElement.prototype.show = function( t ) {
 	// set appropriate display property
 	if( currentStyle.display === "none" ) {
 		// if element remembers it display property it had before element.fadeOut(), restore it, otherwise set it to "block"
-		if( this.dataset.display_before_fade !== "none" ){
-			this.style.display = this.dataset.display_before_fade;
+		if( el.dataset.display_before_fade !== "none" ){
+			el.style.display = el.dataset.display_before_fade;
 		} else {
-			this.style.display = "block";
+			el.style.display = "block";
 		}
 	}
 
 	// fade in + animate height
 	if( currentStyle.opacity < 1 ){
-		let fadeInAnimation = this.animate( [ { opacity: 0, height: 0}, { opacity: 1} ], { duration: t } );
+		let fadeInAnimation = el.animate( [ { opacity: 0, height: 0}, { opacity: 1} ], { duration: t } );
 		fadeInAnimation.addEventListener( "finish", () => {
-			this.style.opacity = 1;
+			el.style.opacity = 1;
 			// restore original overflow if stored, otherwise set it to "auto"
-			if( this.dataset.overflowy_before_fade ) {
-				this.style.overflowY = this.dataset.overflowy_before_fade;
+			if( el.dataset.overflowy_before_fade ) {
+				el.style.overflowY = el.dataset.overflowy_before_fade;
 			} else {
-				this.style.overflowY = "auto";
+				el.style.overflowY = "auto";
 			}
 		} );
 	}
