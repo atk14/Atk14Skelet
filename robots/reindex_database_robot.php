a5df240237d34cb4484bf9e19bc9671e305bfae1
Jaromir Tomek <jaromir.tomek@gmail.com>
2024-06-25 17:18:14 +0200

    ReindexDatabaseRobot improved

--- robots/reindex_database_robot.php
+++ robots/reindex_database_robot.php
@@ -32,6 +32,10 @@ class ReindexDatabaseRobot extends ApplicationRobot {
 			":schema" => $schema
 		);
 
+		$this->dbmole->setErrorHandler(function($_dbmole){
+			throw new Exception("DbMoleException: ".$_dbmole->getErrorMessage());
+		});
+
 		$start_with_table = $arguments["start-with-table"];
 		$start_with_table_q = "";
 		if(strtolower($start_with_table)){
@@ -59,10 +63,40 @@ class ReindexDatabaseRobot extends ApplicationRobot {
 					matviewowner=(SELECT CURRENT_USER)
 			)q$start_with_table_q ORDER BY tablename
 		",$bind_ar);
+
 		foreach($tables as $table){
-			$this->dbmole->doQuery("REINDEX TABLE $table");
-			$this->logger->info("table $table reindexed");
-			$this->logger->flush();
+			$retries = 3;
+			while(1){
+				$exception_thrown = false;
+				try {
+					$this->dbmole->doQuery("REINDEX TABLE $table");
+				}catch(Exception $e){
+					$exception_thrown = true;
+					$message = $e->getMessage();
+					if(preg_match('/deadlock detected/',$message)){
+						$this->logger->info("deadlock detected while reindexing $table");
+						$this->logger->flush();
+					}else{
+						$this->logger->error($message);
+						$this->logger->flush();
+						return;
+					}
+				}
+
+				if(!$exception_thrown){
+					$this->logger->info("table $table reindexed");
+					$this->logger->flush();
+					break;
+				}
+
+				if($retries<=0){
+					$this->logger->info("table $table was NOT reindexed, exiting...");
+					exit; // exit and not return! we don't want the robot lock file to be deleted.
+				}
+
+				sleep(5);
+				$retries--;
+			}
 		}
 	}
 }
