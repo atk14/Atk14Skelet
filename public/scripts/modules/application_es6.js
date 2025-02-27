ffedfdc310cdbbf6b32a7a9af0992dda7c051636
Jakub Tylčer <tylcer@snapps.eu>
2025-02-27 15:56:51 +0100

    photoswipe loader refactor

--- public/scripts/modules/application_es6.js
+++ public/scripts/modules/application_es6.js
@@ -1,98 +1,98 @@
 if( document.querySelector( ".gallery__images, .iobject--picture" ||  document.querySelector( ".js_gallery_trigger" )) ) {
+  initPhotoswipe();
+} else {
+  console.info( "No photoswipe needed on this page" );
+}
+
+function initPhotoswipe() {
   console.log( "trying" );
   try {
-    const m = await import ("/public/dist/scripts/modules/photoswipe-lightbox.esm.min.js" );
+    // Dynamic import of PhotoswipeLightbox module - loads only if needed
+    const m = await import ( "/public/dist/scripts/modules/photoswipe-lightbox.esm.min.js" );
     const PhotoSwipeLightbox = m.default;
-    console.log( "Modul byl úspěšně načten:", PhotoSwipeLightbox );
 
-    const lightbox = new PhotoSwipeLightbox({
+    const lightbox = new PhotoSwipeLightbox( {
       gallery: ".gallery__images, .iobject--picture",
       children: "figure a[data-pswp-width]",
-      pswpModule: () => import("/public/dist/scripts/modules/photoswipe.esm.min.js")
-    });
-    console.log( "Lightbox byl úspěšně inicializován:", lightbox );
+      pswpModule: () => import( "/public/dist/scripts/modules/photoswipe.esm.min.js" )
+    } );
 
-
-
-  // Get image titles and captions
-  lightbox.on( "uiRegister", function() {
-    lightbox.pswp.ui.registerElement( {
-      name: "custom-caption",
-      order: 9,
-      isButton: false,
-      appendTo: "root",
-      html: "",
-      // eslint-disable-next-line no-unused-vars
-      onInit: ( el, pswp ) => {
-        lightbox.pswp.on( "change", () => {
-          const currSlideElement = lightbox.pswp.currSlide.data.element;
-          const parentFigure = currSlideElement.closest( "figure" );
-          const figcaption = parentFigure.querySelector( "figcaption" );
-          let captionHTML = "";
-          if (currSlideElement) {
-            // get caption from alt attribute
-            // captionHTML = currSlideElement.querySelector('img').getAttribute('alt');
-            // Get caption from figcaption tag
-            if( figcaption ) {
-              captionHTML = figcaption.innerHTML;
+    // Get image titles and captions
+    lightbox.on( "uiRegister", function() {
+      lightbox.pswp.ui.registerElement( {
+        name: "custom-caption",
+        order: 9,
+        isButton: false,
+        appendTo: "root",
+        html: "",
+        // eslint-disable-next-line no-unused-vars
+        onInit: ( el, pswp ) => {
+          lightbox.pswp.on( "change", () => {
+            const currSlideElement = lightbox.pswp.currSlide.data.element;
+            const parentFigure = currSlideElement.closest( "figure" );
+            const figcaption = parentFigure.querySelector( "figcaption" );
+            let captionHTML = "";
+            if (currSlideElement) {
+              // get caption from alt attribute
+              // captionHTML = currSlideElement.querySelector('img').getAttribute('alt');
+              // Get caption from figcaption tag
+              if( figcaption ) {
+                captionHTML = figcaption.innerHTML;
+              }
             }
-          }
-          el.innerHTML = captionHTML || "";
-        });
-      }
-    });
-  });
-
-  lightbox.init();
-
-  // Trigger gallery from other links 
-  // Used in gallery on product with variants
-  // and in swiper slider gallery to prevent issues with duplicated slides
-  var galleryTriggers = document.querySelectorAll( ".js_gallery_trigger" );
-
-  galleryTriggers.forEach( function( el ) {
-    el.addEventListener( "click", function( e ) {
-
-      var triggerElement =  e.currentTarget;
-
-      // Get image ID 
-      var imageID = triggerElement.querySelector( "a[data-preview_for]" ).dataset.preview_for;
-      
-      // Find corresponding Photoswipe enabled image link
-      if( triggerElement.closest( ".product-gallery" ) ) {
-        // in gallery for product with variants
-        var galleryLink = triggerElement.closest( ".product-gallery" ).querySelector( ".gallery__item[data-id=\"" + imageID + "\"] a" );
-      } else {
-        // other galleries
-        var galleryLink = triggerElement.closest( ".gallery__images" ).querySelector( "figure[data-gallery_item_id=\"" + imageID + "\"] a");
-      }
-
-      // Trigger click on photoswipe-enabled link
-      if( galleryLink ) {
-        galleryLink.click();
-      }
-
-      e.preventDefault();
+            el.innerHTML = captionHTML || "";
+          } );
+        }
+      } );
     } );
-  } );
 
-  // Trigger photoswipe from link in sinle picture iObject figcaption
-  var pictureTriggers = document.querySelectorAll( ".js_picture_trigger" );
+    lightbox.init();
 
-  pictureTriggers.forEach( function( el ) {
-    el.addEventListener( "click", function( e ) {
-      var imageLink = e.currentTarget.closest( "figure" ).querySelector( "a" );
-      if(imageLink){
-        imageLink.click();
-      }
-      e.preventDefault();
+    // Trigger gallery from other links 
+    // Used in gallery on product with variants
+    // and in swiper slider gallery to prevent issues with duplicated slides
+    var galleryTriggers = document.querySelectorAll( ".js_gallery_trigger" );
+
+    galleryTriggers.forEach( function( el ) {
+      el.addEventListener( "click", function( e ) {
+
+        var triggerElement =  e.currentTarget;
+
+        // Get image ID 
+        var imageID = triggerElement.querySelector( "a[data-preview_for]" ).dataset.preview_for;
+        
+        // Find corresponding Photoswipe enabled image link
+        if( triggerElement.closest( ".product-gallery" ) ) {
+          // in gallery for product with variants
+          var galleryLink = triggerElement.closest( ".product-gallery" ).querySelector( ".gallery__item[data-id=\"" + imageID + "\"] a" );
+        } else {
+          // other galleries
+          var galleryLink = triggerElement.closest( ".gallery__images" ).querySelector( "figure[data-gallery_item_id=\"" + imageID + "\"] a");
+        }
+
+        // Trigger click on photoswipe-enabled link
+        if( galleryLink ) {
+          galleryLink.click();
+        }
+
+        e.preventDefault();
+      } );
     } );
-  } );
 
+    // Trigger photoswipe from link in sinle picture iObject figcaption
+    var pictureTriggers = document.querySelectorAll( ".js_picture_trigger" );
+
+    pictureTriggers.forEach( function( el ) {
+      el.addEventListener( "click", function( e ) {
+        var imageLink = e.currentTarget.closest( "figure" ).querySelector( "a" );
+        if(imageLink){
+          imageLink.click();
+        }
+        e.preventDefault();
+      } );
+    } );
 
   } catch (error) {
-    console.error( "Došlo k chybě při načítání modulu:", error);
+    console.error( "Error loading Photoswipe", error);
   }
-} else {
-  console.info( "Na stránce nejsou žádné galerie nebo obrázky, které by bylo možné zobrazit v lightboxu." );
 }
