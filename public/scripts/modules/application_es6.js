cf6ac115e70f9e36b96da8aec5f5607dd3e277d2
Jakub Tylčer <tylcer@snapps.eu>
2025-03-05 17:45:12 +0100

    swiper js lazy loading in progress

--- public/scripts/modules/application_es6.js
+++ public/scripts/modules/application_es6.js
@@ -1,12 +1,12 @@
 
-//import PhotoSwipeInitializer from "./photoswipe_initializer";
-import SwiperInitializer from "./swiper_initializer";
+//import SwiperInitializer from "./swiper_initializer";
 
 
 //new PhotoSwipeInitializer();
-new SwiperInitializer();
+//new SwiperInitializer();
 
-import PhotoswipeLoader from "./photoswipe_loader.js";
+import PhotoswipeLoader from "./photoswipeloader.js";
+import SwiperLoader from "./swiperloader.js";
 
 // Check if Photoswipe is needed on the page and if so, load it
 if( document.querySelector( ".gallery__images, .iobject--picture" ||  document.querySelector( ".js_gallery_trigger" )) ) {
@@ -16,3 +16,8 @@ if( document.querySelector( ".gallery__images, .iobject--picture" ||  document.q
 }
 // Uncomment next line if you want to expose Photoswipe outside of this module, i.e. if you need to load it manualy
 // Object.assign( globalThis, { PhotoswipeLoader } );
+
+SwiperLoader.load();
+
+// Uncomment next line if you want to expose Swiper outside of this module, i.e. if you need to load it manualy
+//Object.assign( globalThis, { SwiperLoader } );
