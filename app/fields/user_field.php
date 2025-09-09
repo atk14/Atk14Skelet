9e602d54984a95c30ce94e21cbbd4e9922825b77
Jaromir Tomek <jaromir.tomek@gmail.com>
2025-09-09 12:38:55 +0200

    UserField tuned

--- app/fields/user_field.php
+++ app/fields/user_field.php
@@ -1,4 +1,10 @@
 <?php
 class UserField extends ObjectField {
 
+	function __construct($options = array()){
+		parent::__construct($options);
+		$this->update_messages(array(
+			"not_found" => _("Takový uživatel neexistuje"),
+		));
+	}
 }
