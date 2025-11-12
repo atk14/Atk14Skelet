14a78775f0dc7b4d56df8d0373ad4c513635c935
Jakub Tylčer <tylcer@snapps.eu>
2025-11-12 15:24:38 +0100

    js tuning

--- public/admin/scripts/utils/layout_designer.js
+++ public/admin/scripts/utils/layout_designer.js
@@ -20,21 +20,20 @@
 window.UTILS = window.UTILS || { };
 
 window.UTILS.LayoutDesigner = class {
-  designer; // container
-  countSelector; // HTML select element for number of columns
-  // editors for all breakpoints
-  rowXL;
+  designer;                               // container
+  countSelector;                          // HTML select element for number of columns
+  rowXL;                                  // editors for all breakpoints
   rowLG;
   rowMD;
   rowSM;
   rowXS;
-  designerModal; // modal selector
-  columnCount = 0; // current number of columns
-  rows = []; // array for editors
-  copyMDButton; // button to copy Markdown code
-  copyHTMLButton; //  button to copy HTML code
-  storage = sessionStorage; // storage to use - sessionStorage or LocalStorage
-  texts = window.layoutDesignerTexts; // UI strings (rendered in template)
+  designerModal;                          // modal selector
+  columnCount = 0;                        // current number of columns
+  rows = [];                              // array for editors
+  copyMDButton;                           // button to copy Markdown code
+  copyHTMLButton;                         //  button to copy HTML code
+  storage = sessionStorage;               // storage to use - sessionStorage or LocalStorage
+  texts = window.layoutDesignerTexts;     // UI strings (rendered in template)
 
   /**
    * Constructor
@@ -74,18 +73,12 @@ window.UTILS.LayoutDesigner = class {
 
     // Handler for copy Markdown button
     this.copyMDButton.addEventListener( "click", () => {
-      let exportedData = this.exportSizes();
-      console.log( JSON.stringify( exportedData, null, 2 ) );
-      this.setClipboard( this.generateCode( "markdown" ) );
-      this.save();
+      this.codeToClipboard( "markdown" );
     } );
 
     // Handler for copy HTML button
     this.copyHTMLButton.addEventListener( "click", () => {
-      let exportedData = this.exportSizes();
-      console.log( JSON.stringify( exportedData, null, 2 ) );
-      this.setClipboard( this.generateCode( "html" ) );
-      this.save();
+      this.codeToClipboard( "html" );
     } );
 
     // Handler for copy reset button
@@ -247,6 +240,16 @@ window.UTILS.LayoutDesigner = class {
   }
 
   /**
+   * Export code in html or markdown, copies to clipboard, saves layout to storage
+   * @param {String} style - "html"|"markdown"
+   */
+  codeToClipboard( style ) {
+    let exportedData = this.exportSizes();
+    this.setClipboard( this.generateCode( style ) );
+    this.save();
+  }
+
+  /**
    * Puts given text to clipboard
    * @param {String} text 
    */
@@ -269,33 +272,57 @@ window.UTILS.LayoutDesigner = class {
 };
 
 
+
+
+
+/**
+ * Helper class for single breakpoint editor called row editor
+ */
 window.UTILS.LayoutDesignerRow = class {
-  rowEditor;
-  cellsContainer;
-  cells = [];
-  hiddenCellsContainer;
-  stacked;
-  btnCopyUp;
-  btnCopyDown;
+  rowEditor;              // html of this class
+  cellsContainer;         // html container for displaying cells
+  cells = [];             // array to store cells
+  hiddenCellsContainer;   // html container to display hidden cells
+  stacked;                // if true all cells will span full width by default
+  btnCopyUp;              // copy button
+  btnCopyDown;            // paste button
+  texts = window.layoutDesignerTexts; // UI strings (rendered in template)
   
+  /**
+   * 
+   * @param {*} id - id attribute for editor
+   * @param {*} title - html title for editor
+   * @param {*} stacked - if true, all cells will span full width by default
+   */
   constructor( id, title, stacked ) {
+    // create row
     this.rowEditor = document.createElement( "div" );
     document.querySelector( "#layout_designer_modal .editor-container").appendChild( this.rowEditor );
+
+    // editor content from html template tag
     let editorFragment = document.querySelector( "#layout_designer_row" ).content.cloneNode( true );
     this.rowEditor.appendChild( editorFragment );
     this.rowEditor.setAttribute( "id", id );
+
+    // reference html nodes
     this.cellsContainer = this.rowEditor.querySelector( ".row-edit-area" );
     this.hiddenCellsContainer = this.rowEditor.querySelector( ".hidden-cells-container" );
     this.btnCopy = this.rowEditor.querySelector( ".js--btn-copy" );
     this.btnPaste = this.rowEditor.querySelector( ".js--btn-paste" );
 
+    // set title
     this.rowEditor.querySelector( ".js--row-title" ).innerHTML = title;
    
-    this.cells = []; console.log("cl", this.cells.length);
+    this.cells = [];
     this.stacked = stacked;
     this.copyPasteHandlers();
   }
 
+  /**
+   * Add new cell
+   * @param {int} colspan 1-12
+   * @returns {LayoutDesignerCell}
+   */
   addCell( colspan ) {
     if( this.stacked ) {
       colspan = 12;
@@ -306,6 +333,9 @@ window.UTILS.LayoutDesignerRow = class {
     return cell;
   }
 
+  /**
+   * Remove last cell
+   */
   removeLastCell() {
     let lastCell = this.cells.pop();
     if ( lastCell ) {
@@ -313,12 +343,18 @@ window.UTILS.LayoutDesignerRow = class {
     }
   }
 
+  /**
+   * Remove all cells
+   */
   clear() {
     this.cells.forEach( cell => cell.destroy() );
     this.cells = [];
     this.cellsContainer.innerHTML = "";
   }
 
+  /**
+   * Set even widths to cells to fit in one row
+   */
   evenWidths() {
     let evenspan = Math.floor( 12 / this.cells.length );
     if( this.stacked ) {
@@ -329,6 +365,10 @@ window.UTILS.LayoutDesignerRow = class {
     } );
   }
 
+  /**
+   * Getter to get all cells spans
+   * @returns {Array}
+   */
   get sizes(){
     let widths = []
     this.cells.forEach(
@@ -340,9 +380,13 @@ window.UTILS.LayoutDesignerRow = class {
     return widths;
   }
 
+  /**
+   * Setter to set cell widths
+   * @param {Array} newsizes 
+   */
   set sizes ( newsizes ) {
     if( newsizes.length !== this.cells.length ) {
-      alert( "Cannot paste columns: Number of pasted columns do not match number of current columns." );
+      alert( this.texts.wrongPaste );
       return;
     }
     for( let i=0; i < newsizes.length; i++ ) {
@@ -350,6 +394,9 @@ window.UTILS.LayoutDesignerRow = class {
     }
   }
 
+  /**
+   * Handlers for copy/paste breakpoint layout
+   */
   copyPasteHandlers() {
     console.log(this.sizes)
     this.btnCopy.addEventListener( "click", ()=>{
@@ -362,22 +409,35 @@ window.UTILS.LayoutDesignerRow = class {
 };
 
 
+
+/**
+ * Helper class for single cell (column)
+ */
 window.UTILS.LayoutDesignerCell = class {
-  element;
-  #span;
-  cellNumber;
-  hiddenCellAvatar = null;
+  element;                    // this html 
+  #span;                      // cell span 1-12 
+  cellNumber;                 // order number of this cell
+  hiddenCellAvatar = null;    // graphic representation of this cell when hidden
   previousSpan;
 
+  /**
+   * 
+   * @param {LayoutDesignerRow} parent 
+   * @param {int} colspan -  cell span 
+   * @param {int} cellNumber - cell order number 
+   */
   constructor( parent, colspan, cellNumber ) {
     this.parent = parent;
     this.cellNumber = cellNumber;
     this.element = document.createElement( "div" );
+    // get UI controls from html template tag
     let controls = document.querySelector( "#layout_designer_cell_controls" ).content.cloneNode( true );
     this.element.appendChild( controls );
 
+    // display cell number
     this.element.querySelector( ".cellnumber" ).innerHTML = cellNumber;
 
+    // set UI handlers
     this.element.querySelector( ".js-span-plus" ).addEventListener( "click", () => {
       if ( this.#span < 12 ) {
         this.span += 1;
@@ -391,9 +451,15 @@ window.UTILS.LayoutDesignerCell = class {
     this.element.querySelector( ".js-hide-cell" ).addEventListener( "click", () => {
         this.span =0;
     } );
+
+    // set initial span
     this.span = colspan;
   }
 
+  /**
+   * Setter for cell span
+   * @param {int} span - 1-12
+   */
   set span ( n ) {
     this.previousSpan = this.#span;
     this.#span = n;
@@ -413,13 +479,21 @@ window.UTILS.LayoutDesignerCell = class {
     }
   }
 
+  /**
+   * getter for cell span
+   * @returns {int} - cell span
+   */
   get span() {
     return this.#span;
   }
 
+  /**
+   * Create hidden cell graphics representation
+   */
   createHiddenCellAvatar() {
     console.log( "cell hidden, No. ", this.cellNumber );
     let hiddenCellAvatar = document.createElement( "div" );
+    // get markup from html template
     let content = document.querySelector( "#hidden_cell_avatar" ).content.cloneNode( true );
     hiddenCellAvatar.className = "hidden_cell_avatar";
     hiddenCellAvatar.appendChild( content );
@@ -436,6 +510,9 @@ window.UTILS.LayoutDesignerCell = class {
 
   }
 
+  /**
+   * Destroys hidden cell avatar (when cell is unhidden or is deleted)
+   */
   destroyHiddenCellAvatar() {
     if( this.hiddenCellAvatar && this.parent.hiddenCellsContainer.contains( this.hiddenCellAvatar ) ) {
       this.parent.hiddenCellsContainer.removeChild( this.hiddenCellAvatar );
@@ -443,7 +520,9 @@ window.UTILS.LayoutDesignerCell = class {
     this.hiddenCellAvatar = null;
   }
 
-
+  /**
+   * Destroy this cell
+   */
   destroy() {
     if( this.hiddenCellAvatar ){
       this.destroyHiddenCellAvatar( this.hiddenCellAvatar );
